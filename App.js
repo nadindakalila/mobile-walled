7a5ea8b573471d573a47c1e89873bb3c531957d0
Fariz <fariz.rizaldy@salt.co.id>
2024-12-15 16:25:56 +0700

    validasi done

--- App.js
+++ App.js
@@ -1,24 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, ImageBackground, ScrollView, Button, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View, Image, ImageBackground, ScrollView, Button, SafeAreaView, Switch } from 'react-native';
 import Box from './components/Box';
+import Form from './components/Form';
+import React, {useState} from 'react';
+
+
 
 export default function App() {
   return (
-    <SafeAreaView>
-      <View style={styles.container}>
-      <View style={{flexDirection: 'row', elevation: 3, paddingHorizontal: 20, display: 'flex', alignItems: 'center', height: 80, width: '100%'}}>
-        <Image source={require('./assets/icon.png')} style={{width: 46, height: 46}}></Image>
-        <View style={{ marginLeft: 20}}>
-          <Text style={{color: 'red', fontWeight: 700}}>Andika</Text>
-          <Text >Personal Account</Text>
-        </View>
-        <View style={{flex: 1}}>
-
-        </View>
-        <Image source={require('./assets/splash-icon.png')} style={{width: 46, height: 46}}></Image>
-      </View>
-    </View>
-    </SafeAreaView>
+      <SafeAreaView style={styles.container}>
+        <Form></Form>
+      </SafeAreaView>
   );
 }
 
---
+++ components/Form.js
@@ -0,0 +1,69 @@
+import { StyleSheet, SafeAreaView, TextInput, Button, Text } from 'react-native';
+import React, {useState} from 'react';
+
+export default function Form() {
+  const [text, onChangeText] = useState('Useless Text');
+  const [number, onChangeNumber] = useState('');
+  const [errors, setErrors] = useState({}); // untuk mengatur error masing2 form
+  const validate = () => { // function validasi untk mengatur jika user tidak mengisi input
+    let errors = {};
+    if (!text) {
+      errors.text = 'Text is required';
+    }
+    if (!number) {
+      errors.number = 'Number is required';
+    }
+    setErrors(errors);
+    return Object.keys(errors).length === 0;
+  };
+
+  const handleSubmit = () => { // validasi
+    if (validate()) {
+      alert('Form submitted successfully!');
+    }
+  };
+  return (
+      <SafeAreaView style={styles.container}>
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeText}
+          value={text}
+        />
+        {errors.text && <Text style={styles.errorText}>{errors.text}</Text>}
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeNumber} // Menangkap perubahan dari inputan user secara real time
+          value={number} // Untuk mengatur nilai inputan
+          placeholder="useless placeholder" // Menampilkan teks sementara yang memberi petunjuk kepada pengguna tentang input yang diharapkan
+          keyboardType="numeric" // Untuk menentukan tipe keyboard.
+          secureTextEntry // Digunakan untuk menyembunyikan input (misalnya untuk password)
+        />
+        <Button title="Submit" onPress={handleSubmit} />
+      </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+  },
+  input: {
+    width: '100%',
+    height: 50,
+    borderColor: '#ddd',
+    borderWidth: 1,
+    borderRadius: 15,
+    paddingHorizontal: 10,
+    marginBottom: 15,
+    backgroundColor: '#f9f9f9',
+    fontSize: 16,
+  },
+  errorText: {
+    color: 'red',
+    marginBottom: 10,
+  }
+});
